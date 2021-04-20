784773e9d7ca89606dd3a225dd1d4101464c3483
Louie Schoenknecht <luisesch97@gmail.com>
2021-04-20 15:01:44 -0700

    add tasks component to app

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,11 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Doctors appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Meeting at school',
-    day: 'Feb 6th at 1:30pm',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Food shopping',
-    day: 'Feb 5th at 2:30pm',
-    reminder: false,
-  }
-]
 
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
+  
   return (
     <>
-      {tasks.map((task) => (<h3>{task.text}</h3>))}
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
     </>
   )
 }
